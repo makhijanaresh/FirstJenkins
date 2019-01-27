fdb33148f1fdd76d05153898394e685e272df1cb
makhijanaresh <makhijanaresh@hotmail.com>
2019-01-27 12:09:30 +0530

    Fourth with exception

--- one.java
+++ one.java
@@ -6,5 +6,6 @@ public static void main(String gg[])
 System.out.println("Date is:"+new Date());
 System.out.println("Updated");
 System.out.println("Update 2");
+throw new RuntimeException("Exception for testing jendkins fail feature");
 }
 }
